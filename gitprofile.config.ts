4121d325ffa167cbbb2478fcd2df232605979b8f
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2024-06-25 20:35:04 -0400

    Disable theme option

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -174,11 +174,11 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dum',
+    defaultTheme: 'bumblebee',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
